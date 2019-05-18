d2be5d7cbf74ba31e9a1341893a19ac72f2f05b1
Sylvance <kerandisylvance@gmail.com>
2019-05-18 22:46:42 +0300

    Add intro title

---
+++ colors.sh
@@ -0,0 +1,28 @@
+# COLORS
+
+# Black           0;30  Dark Gray       1;30
+# Dark Red        0;31  Light Red       1;31
+# Dark Green      0;32  Light Green     1;32
+# Dark Yellow     0;33  Light Yellow    1;33
+# Dark Blue       0;34  Light Blue      1;34
+# Dark Purple     0;35  Light Purple    1;35
+# Dark Cyan       0;36  Light Cyan      1;36
+# Light Gray      0;37  White           1;37
+
+NO_COLOR='\033[0m'
+DARK_RED='\033[0;31m'
+LIGHT_RED='\033[1;31m'
+DARK_GREEN='\033[0;32m'
+LIGHT_GREEN='\033[1;32m'
+DARK_YELLOW='\033[0;33m'
+LIGHT_YELLOW='\033[1;33m'
+DARK_BLUE='\033[0;34m'
+LIGHT_BLUE='\033[1;34m'
+DARK_PURPLE='\033[0;35m'
+LIGHT_PURPLE='\033[1;35m'
+DARK_CYAN='\033[0;36m'
+LIGHT_CYAN='\033[1;36m'
+DARK_GRAY='\033[0;37m'
+LIGHT_GRAY='\033[1;37m'
+
+# echo -e "${LIGHT_GREEN}################################################################################################${NO_COLOR}\n"
